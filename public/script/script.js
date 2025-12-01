4434aa9259221167a6689aa55edcd0b1b6a9fb17
NRG55 <maloj_1980@yahoo.fr>
2025-12-01 08:54:04 +0000

    Update: filter (add a mobile version). Update: css.

--- public/script/script.js
+++ public/script/script.js
@@ -1,31 +1,56 @@
 const header = document.querySelector("header");
-const button = document.querySelector(".menu-button");
-const open = document.querySelector(".open-button");
+const openMenuButton = document.querySelector(".open-menu-button");
+const openFilterButton = document.querySelector(".open-filter-button");
+const filter = document.querySelector(".filter");
+const searchForm = document.querySelector(".search-form");
 
 function toggleMenu() {
-    button.addEventListener("click", () => {
-        button.classList.toggle("active");       
+    openMenuButton.addEventListener("click", () => {
+        openMenuButton.classList.toggle("active");       
         const isActive = header.classList.toggle("menu-active");
       
         if (isActive) {
-            open.setAttribute("aria-expanded", "true");
+            openMenuButton.setAttribute("aria-expanded", "true");
         } else {
-            open.setAttribute("aria-expanded", "false");
+            openMenuButton.setAttribute("aria-expanded", "false");
         };
     });
 };
 
-function escapeMenu() {
-	document.addEventListener("keydown", (e) => {
-		if (e.key === "Escape" && header.classList.contains("menu-active")) {
-			header.classList.remove("menu-active");
-            button.classList.remove("active");
-			open.setAttribute("aria-expanded", "false");
+function openFilter() {
+    openFilterButton.addEventListener("click", () => {
+        filter.classList.add("filter-active");
+        searchForm.classList.add("search-form-active");          
+        openFilterButton.setAttribute("aria-expanded", "true");       
+    });
+};
+
+function escapeMenu() {   
+	document.addEventListener("keydown", (e) => {     
+		if (e.key === "Escape" && (header.classList.contains("menu-active") || filter.classList.contains("filter-active"))) {		
+            header.classList.remove("menu-active");
+            openMenuButton.classList.remove("active");
+			openMenuButton.setAttribute("aria-expanded", "false");
+            filter.classList.remove("filter-active");
+            searchForm.classList.remove("search-form-active");          
+            openFilterButton.setAttribute("aria-expanded", "false");
 		};
 	});
 };
 
+window.onresize = function () {
+    let w = window.outerWidth;
+    if (w > 768) {
+        header.classList.remove("menu-active");
+        openMenuButton.classList.remove("active");
+        filter.classList.remove("filter-active");
+        searchForm.classList.remove("search-form-active");          
+        openFilterButton.setAttribute("aria-expanded", "false");
+    };
+};
+
 document.addEventListener("DOMContentLoaded", () => {
 	toggleMenu();
+    openFilter();
 	escapeMenu();
 });
